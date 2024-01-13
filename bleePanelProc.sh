656158444af4014a00c3f02518e94af9df033616
Mohsen Banan <form@mohsen.banan.1.byname.net>
2024-01-12 16:13:11 -0800

    Common.

--- bleePanelProc.sh
+++ bleePanelProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedBleePanelProc.sh]] |
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/overview/bleePanelProc.sh
+*  /This File/ :: /bisos/panels/overview/bleePanelProc.sh
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedBleePanelProc.sh -l $0 "$@"
